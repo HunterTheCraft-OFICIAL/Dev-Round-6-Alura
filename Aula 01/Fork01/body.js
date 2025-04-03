c0da64c2a815de7ba9d11e9204abc3c1d81a92fa
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-03 15:00:56 -0300

    primeiro commit

--- Aula 01/Fork01/body.js
+++ Aula 01/Fork01/body.js
@@ -1,4 +1,7 @@
 
+Quero Melhorar isso exemplo quero mostrar os valores formatados corretamente com às pontuações corretas para ficar bonito visualmente independente do valor. Ps: o Câmbio e em relação ao Real de um Projeto temático de uma Imersão Dev_ da Alura
+código:
+
 valorwons = prompt("Digite um valor em wons:")
 //alert(valorwons)
 
