3b537d001dc913cedfb58888cce4aa016bd46c79
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-02 21:31:18 -0300

    primeiro commit

--- teste.js
+++ teste.js
@@ -2,11 +2,18 @@ console.log("oi")
 
 //Não funciona fora dos consoles dos navegadores como Chrome 
 //alert("Olá Mundo!")
+//alert(44 / 3)
 
 //10 + 2
+console.log(10 + 2)
 //45-3
+console.log(45-3)
 //14 * 2
+console.log(14 * 2)
 //44 / 3
+console.log(44 / 3)
 
 
-alert(44 / 3)
+
+
+
