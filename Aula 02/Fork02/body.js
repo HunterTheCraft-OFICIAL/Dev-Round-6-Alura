1b38a2aaf41c76819df29d5d2891b0e682109bea
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-04 20:47:55 -0300

    primeiro commit

--- Aula 02/Fork02/body.js
+++ Aula 02/Fork02/body.js
@@ -1,50 +1,37 @@
-idade = prompt("Qual a sua Idade?")
-
-if (idade < 18) {
-  alert("Espere! você NÃO pode jogar Jokenpo")
-} 
-
-if (idade >= 18) {
-  alert("Parabéns você pode jogar Jokenpo")
-  
-  escolhaJogador = prompt("Faça sua escolha. 1 - Pedra, 2 - Papel, 3 - Tesoura")
-  
-  escolhaComputador = Math.floor(Math.random() * 3)+1
-  
-  // Cenário - 01
-  // Pedra + Pedra = Empate ✓
-  // Papel + Papel = Empate ✓
-  // Tesoura + Tesoura = Empate ✓
-  
-  if (escolhaJogador == escolhaComputador) {
-    alert(Empate no Jogo)
-  }
-  
-  // Cenário - 02
-  // Pedra + Pedra = Empate ×
-  // Pedra + Papel = Perde ✓
-  // Pedra + Tesoura = Vence ✓
-  
-  if (escolhaJogador == 1) {
-  
-  }
-  
-  // Cenário - 3
-  // Papel + Pedra = Vence ✓
-  // Papel + Papel = Empate ×
-  // Papel + Tesoura = Perde ✓
+  // Papel + Pedra = Vence ✓ 1
+  // Papel + Papel = Empate × 2
+  // Papel + Tesoura = Perde ✓ 3
   
   if (escolhaJogador == 2) {
-  
+    
+    // Papel + Pedra = Vence 
+    if(escolhaComputador == 1) {
+      alert("O Jogador Venceu! o Computador escolheu:" escolhaComputador)
+    }
+    
+    // Papel + Tesoura = Perde 
+    if(escolhaComputador == 3 {
+       alert("O Computador Venceu! o Computador escolheu:" escolhaComputador)
+       }
+    
   }
   
-  // Cenário - 4
-  // Tesoura + Pedra = Vence ✓
-  // Tesoura + Papel = Perde ✓
-  // Tesoura + Tesoura = Empate ×
+  // Cenário - 4 (Escolha 03)
+  // Tesoura + Pedra = Vence ✓ 1
+  // Tesoura + Papel = Perde ✓ 2
+  // Tesoura + Tesoura = Empate × 3
   
   if (escolhaJogador == 3) {
-  
+    
+    // Tesoura + Pedra = Vence
+    if(escolhaComputador == 1) {
+      alert("O Jogador Venceu! o Computador escolheu:" escolhaComputador)
+    }
+      
+    // Tesoura + Papel = Perde 
+    if(escolhaComputador == 2) {
+      alert("O Computador Venceu! o Computador escolheu:" escolhaComputador)
+    }
   }
   
 }
