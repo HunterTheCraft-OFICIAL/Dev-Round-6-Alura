1680dcd32c452412c23ac37b15fac5d196d8d34d
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-03 23:42:46 -0300

    primeiro commit

--- Aula 02/Fork02/body.js
+++ Aula 02/Fork02/body.js
@@ -1,4 +1,11 @@
 idade = prompt("Qual a sua Idade?")
+
+if (idade < 18) {
+  alert("Espera! você NÃO pode Jogar Jokenpo")
+} 
+
 if (idade >= 18) {
   alert("Parabéns você pode Jogar Jokenpo")
 }
+
+//
