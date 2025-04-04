86e1b0c327b1e8a934954215641abe34698b390b
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-03 23:07:55 -0300

    primeiro commit

---
+++ Aula 02/Fork02 (01)/body.js
@@ -0,0 +1,12 @@
+
+let valorwons = prompt("Digite um Valor em Wons")
+//alert(valorwons)
+
+umwon = 0.0040
+//alert("R$" + valorwons * umwon)
+
+let valorReal = valorwons * umwon
+
+let valorFormatado = valorReal.toLocaleString('BTL', {style: 'currency', currency: 'BRL'});
+
+alert(valorFormatado)
