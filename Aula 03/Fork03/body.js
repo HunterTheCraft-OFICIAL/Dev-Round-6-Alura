c8f7c75784de974e44039bf4a39df6e0d682804b
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-05 00:17:51 -0300

    primeiro commit

--- Aula 03/Fork03/body.js
+++ Aula 03/Fork03/body.js
@@ -1,22 +1,39 @@
 //Novo Jogo
 
-rodada = 1
+rodada = 1;
 
-while(rodada <= 3) {
-  console.log("Rodada: " + rodada)
-//  rodada = rodada + 1
-  
-  escolhaJogador = prompt("Rodada: "+rodada+", Escolha (1, 2 ou 3)")
-  pisoQuebrado = Math.floor(Math.random() * 3) + 1;
-  
-  if(escolhaJogador == pisoQuebrado) {
-    //Falhou na Escolha e caiu no limbo 
-    alert("Você caiu no Limbo pois o Piso " + pisoQuebrado + " estava Quebrando")
-    rodada = 1000
-  }
-  
-  rodada = rodada + 1
-  
+while (rodada <= 3) {
+console.log("Rodada: " + rodada);
+
+// rodada = rodada + 1
+
+escolhaJogador = prompt("Rodada: " + rodada + ", Escolha (1, 2 ou 3)");
+
+pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+
+if (escolhaJogador == pisoQuebrado) {
+//Falhou na Escolha e caiu no limbo
+
+alert("Você caiu no Limbo pois o Piso " + pisoQuebrado + " estava Quebrando");
+
+rodada = 1000; 
+} else {
+//Passou com Dificuldades
+
+alert("O Piso Quebrado ficou no " + pisoQuebrado + ".");
+} else {
+
+alert("Você NÃO sobreviveu");
+
 }
+
+rodada = rodada + 1;
+}
+
+if (rodada == 4) {
+    alert("Você Sobreviveu por pouco!");
+} 
+
+//rodada = 4
 
 //Rodada 4
