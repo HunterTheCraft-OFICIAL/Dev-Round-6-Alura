86ca9c0834b600fd47c7898b74b3c48c4218abce
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-05 02:07:31 -0300

    primeiro commit

--- Aula 04/Fork03/body.js
+++ Aula 04/Fork03/body.js
@@ -1,21 +1,3 @@
-//Novo Jogo
-
-//rodada = 1;
-
-//while (rodada <= 3) {
-
-for(let rodada = 1; rodada <= 3; rodada++){
-  console.log("Rodada: " + rodada);
-
-  // rodada = rodada + 1
-
-  escolhaJogador = prompt("Rodada: " + rodada + ", Escolha (1, 2 ou 3)");
-
-  pisoQuebrado = Math.floor(Math.random() * 3) + 1;
-
-  if (escolhaJogador == pisoQuebrado) {
-    //Falhou na Escolha e caiu no limbo
-
     alert(
       "Você caiu no Limbo pois o Piso " + pisoQuebrado + " estava Quebrando"
     );
@@ -25,12 +7,14 @@ for(let rodada = 1; rodada <= 3; rodada++){
     //Passou com Dificuldades
 
     alert("O Piso Quebrado ficou no " + pisoQuebrado + ".");
-  } 
 
-//rodada = rodada + 1;
+    ganhou = 1;
+  }
+
+  //rodada = rodada + 1;
 }
 
-if (rodada == 4) {
+if (ganhou == 1) {
   alert("Você Sobreviveu por pouco!");
 } else {
   alert("Você NÃO sobreviveu");
@@ -41,4 +25,4 @@ if (rodada == 4) {
 //Rodada 4
 
 //Fim da Aula 03
-//Mudanças na Aula 04
+//Mais conteúdo na Aula 04
