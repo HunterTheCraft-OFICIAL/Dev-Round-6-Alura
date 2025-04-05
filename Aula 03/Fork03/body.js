ef2e5837b9647e993efa70a50e58f4bf2d67b01c
HunterTheCraft-OFICIAL <impahunter67@gmail.com>
2025-04-04 23:34:10 -0300

    primeiro commit

--- Aula 03/Fork03/body.js
+++ Aula 03/Fork03/body.js
@@ -4,10 +4,12 @@ rodada = 1
 
 while(rodada <= 3) {
   console.log("Rodada: " + rodada)
-  rodada = rodada + 1
+//  rodada = rodada + 1
   
-  escolhaJogador = prompt("Rodada:"+rodada+", Escolha (1, 2 ou 3)")
-  pisoQuebrado = Math.floor(Math.randon() * 3) + 1;
+  escolhaJogador = prompt("Rodada: "+rodada+", Escolha (1, 2 ou 3)")
+  pisoQuebrado = Math.floor(Math.random() * 3) + 1;
+  
+  rodada = rodada + 1
   
 }
 
